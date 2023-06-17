e2303d4384b37dfd12954d19ca3101a2e9c38eec
NAJMUNISHAA D <132421295+Najmunisha@users.noreply.github.com>
2023-06-17 16:32:50 +0530

    Add files via upload

---
+++ Phase 3 Assisted project 3/Assisted Project 7/SpringRESTapi/src/main/java/com/test/example/SpringResTapiApplication.java
@@ -0,0 +1,13 @@
+package com.test.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResTapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResTapiApplication.class, args);
+	}
+
+}
